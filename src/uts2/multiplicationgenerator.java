42703250b7aa7f7e0f48735811baca9bb351336c
rinanurrizqi <rinanurrizqi@gmail.com>
2024-05-14 19:23:38 +0700

    multiplication generator

--- src/uts2/multiplicationgenerator.java
+++ src/uts2/multiplicationgenerator.java
@@ -9,5 +9,26 @@ package uts2;
  * @author UsEr
  */
 public class multiplicationgenerator {
+    private final int a;
+    private int b;
     
+    public multiplicationgenerator(int a){
+        this.a = a;
+        this.b = 1;
+    }
+    public boolean hasNext(){
+        return true;
+    }
+    public String next(){
+        String result = a + "x" + b + "=" +(a*b);
+        b++;
+        return result;
+    }
+    public static void main(String[] args) {
+        multiplicationgenerator generator = new multiplicationgenerator(4);
+        
+        for(int i=0; i<10; i++){
+            System.out.println(generator.next());
+        }
+    }
 }
