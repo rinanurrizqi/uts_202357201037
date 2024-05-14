3287e6ae4c3bdbaabdb0fa06bea89b7fb6b08e0c
rinanurrizqi <rinanurrizqi@gmail.com>
2024-05-14 19:45:49 +0700

    first commit

---
+++ src/uts2/shortestword.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uts2;
+
+/**
+ *
+ * @author UsEr
+ */
+public class shortestword {
+    public static void main(String[] args) {
+        String input = "This is a simple test string";
+        int shortestLength = findShortestWordLength(input);
+        System.out.println("The length of the shortest word is: " + shortestLength);
+    }
+    public static int findShortestWordLegth(String str){
+        String words = str.split("");
+        int minLength = 
+    }
+}
