aeacbbe7f2dd3df35b7b7e4ceba772f3149f8a9a
rinanurrizqi <rinanurrizqi@gmail.com>
2024-05-14 17:57:40 +0700

    first commit

--- src/uts2/arithmophobia.java
+++ src/uts2/arithmophobia.java
@@ -9,19 +9,4 @@ package uts2;
  * @author UsEr
  */
 public class arithmophobia {
-    public static void main(String[] args) {
-        String phrase = "Arithmophobia";
-        
-        if (phrase.matches(".*\\d+.*")){
-            System.out.println("Angka yang ditakuti");
-        }else {
-            System.out.println("");
         }
-        
-        if (phrase.contains("13")){
-            System.out.println("Bukan angka 13");
-        }else {
-            System.out.println("Angka 13 tidak membuatku takut");
-        }
-    }
-}
